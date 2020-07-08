faa483632cc23dd965e4b901de396aec550902ad
michaelacorn <michaelacorn@gmail.com>
2020-07-08 16:44:30 -0400

    Catch up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'To Do'
+  // toDoEntry = ' '
+  todo: object[];
+  todoTitle: string; 
+  completed: boolean = true;
+
+
+  // constructor () {}
+
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todo = [
+      {
+       'id': 1, 
+       'item': 'Fold Clothes',
+       'completed': true,
+      },
+      {
+        'id': 1, 
+        'item': 'Put clothes in Dresser',
+        'completed': false,
+       },
+       {
+        'id': 1, 
+        'item': 'Relax',
+        'completed': false,
+       },
+       {
+        'id': 1, 
+        'item': 'Try To Pet Cat',
+        'completed': false,
+       },
+       {
+        'id': 1, 
+        'item': 'Pet Dog',
+        'completed': true,
+       },
+       {
+        'id': 1, 
+        'item': 'Be Awesome',
+        'completed': true,
+       },
+      ]
+  }
+
 }
+
